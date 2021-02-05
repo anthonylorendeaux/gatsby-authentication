64465391371d565ee223ea4443d5855d0714e72c
Anthony Lorendeaux <anthony.lorendeaux@free.fr>
2021-02-05 22:27:14 +0100

    :sparkles: Add logout
    Add logout button in the header.

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,70 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import useAuth from "../hooks/useAuth"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+const Header = ({ siteTitle }) => {
+  const { logout, isAuthenticated } = useAuth()
+
+  const handleLogout = (e) => {
+    e.preventDefault()
+    logout()
+  }
+
+  return (
+    <header
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        background: `rebeccapurple`,
+        marginBottom: `1.45rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+        }}
+      >
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+      </div>
+      <div>
+        { isAuthenticated 
+          ? (
+              <a 
+                onClick={handleLogout}
+                style={{
+                  color: `white`,
+                  textDecoration: `none`,
+                }}
+                href="/">
+                Logout
+              </a>  
+            )
+          : (
+            <Link
+              to="/login"
+              style={{
+                color: `white`,
+                textDecoration: `none`,
+              }}>
+                Login
+              </Link>
+            )
+        }
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
@@ -38,5 +73,6 @@ Header.propTypes = {
 Header.defaultProps = {
   siteTitle: ``,
 }
+
 
 export default Header
